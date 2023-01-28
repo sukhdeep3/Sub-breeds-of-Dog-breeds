51a548496812ffb66aece1828a864a343d1b6645
sukhdeep3 <sukhdeepsukh589@gmail.com>
2023-01-28 04:01:47 -0500

    SubBreeds are working
    Signed-off-by: sukhdeep3 <sukhdeepsukh589@gmail.com>

--- Doggallery.js
+++ Doggallery.js
@@ -2,13 +2,9 @@ let dropdown = $("#dropdown");
 let imagebtn = $("#image-btn");
 let breed;
 
-//  console.log("Working");
 $.get("https://dog.ceo/api/breeds/list/all", function (data) {
-  // console.log('Hello');
   let list = data.message;
-
   for (const breed in list) {
-    // console.log(breed);
     dropdown.append(`<option value="${breed}"> ${breed} <option>`);
   }
 });
@@ -22,39 +18,37 @@ dropdown.change(function () {
   $.get(imgURL, function (data) {
     if (data.message.length !== 0) {
       let subBreeds = data.message;
+      const arr = [];
+
+      for (let key in subBreeds) {
+        arr.push(subBreeds[key]);
+      }
 
       dropdown.after('<select id="sub-breed" style="padding: 6px;"> </select>');
 
       var subDropdown = $("#sub-breed");
 
-      for (const subBreed of subBreeds) {
-        subDropdown.append(
-          '<option value="' +
-            subBreed +
-            '">' +
-            subBreed +
-            "</option>"
-        );
-        // console.log(subBreed);
-      }
+      arr.map((sub) => {
+        for (const subBreed of sub) {
+          subDropdown.append(
+            '<option value="' + subBreed + '">' + subBreed + "</option>"
+          );
+        }
+      });
     }
   });
-  // console.log(imgURL);
 });
 
 imagebtn.click(function (e) {
   e.preventDefault();
-  // console.log("hlo");
 
   breed = dropdown.val();
 
   let subBreed = $("#sub-breed").val();
-  //console.log(subBreed);
   let imgURL = "https://dog.ceo/api/breed/" + breed;
 
   if (subBreed !== undefined) {
     imgURL += "/" + subBreed;
-    // console.log('Hello1');
   }
   imgURL += "/images";
 
@@ -62,7 +56,6 @@ imagebtn.click(function (e) {
 
   $.get(imgURL, function (data) {
     let breedImg = data.message;
-    // console.log(breedImg);
     for (let imageUrl of breedImg) {
       $("#lower-container").append(
         '<img src="' + imageUrl + '" alt="' + breed + '">'
